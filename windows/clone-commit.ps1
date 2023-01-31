d2deee9dc478dc625b3c5b53252ea0e230dfb935
s-weigand <s.weigand.phy@gmail.com>
2023-01-31 16:06:13 +0100

    🩹 Fix typo in console log print

--- windows/clone-commit.ps1
+++ windows/clone-commit.ps1
@@ -1,7 +1,7 @@
 
 Set-Variable -Name "FLAGS" -Value ""
 if ($Env:PLUGIN_DEPTH) {
-    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH" 
+    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH"
 }
 
 if (!(Test-Path .git)) {
@@ -13,5 +13,5 @@ if (!(Test-Path .git)) {
 
 Write-Host "git fetch $FLAGS origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
 git fetch $FLAGS origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
-Write-Host "git checkout $Env:DRONE_COMMIT_SHA -f $Env:DRONE_COMMIT_BRANCH";
+Write-Host "git checkout $Env:DRONE_COMMIT_SHA -b $Env:DRONE_COMMIT_BRANCH";
 git checkout $Env:DRONE_COMMIT_SHA -b $Env:DRONE_COMMIT_BRANCH;
--- windows/windows_gen.go
+++ windows/windows_gen.go
@@ -73,7 +73,7 @@ switch ($Env:DRONE_BUILD_EVENT) {
 const CloneCommit = `
 Set-Variable -Name "FLAGS" -Value ""
 if ($Env:PLUGIN_DEPTH) {
-    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH" 
+    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH"
 }
 
 if (!(Test-Path .git)) {
@@ -85,7 +85,7 @@ if (!(Test-Path .git)) {
 
 Write-Host "git fetch $FLAGS origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
 git fetch $FLAGS origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:";
-Write-Host "git checkout $Env:DRONE_COMMIT_SHA -f $Env:DRONE_COMMIT_BRANCH";
+Write-Host "git checkout $Env:DRONE_COMMIT_SHA -b $Env:DRONE_COMMIT_BRANCH";
 git checkout $Env:DRONE_COMMIT_SHA -b $Env:DRONE_COMMIT_BRANCH;
 `
 
@@ -112,7 +112,7 @@ git merge $Env:DRONE_COMMIT_SHA
 const CloneTag = `
 Set-Variable -Name "FLAGS" -Value ""
 if ($Env:PLUGIN_DEPTH) {
-    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH" 
+    Set-Variable -Name "FLAGS" -Value "--depth=$Env:PLUGIN_DEPTH"
 }
 
 if (!(Test-Path .git)) {
